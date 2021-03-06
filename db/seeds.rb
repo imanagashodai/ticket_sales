9556c6bd98a066878ef444edc1c06b7685a5e6fb
kohei <msk_mkt_tdf_ohr@yahoo.co.jp>
2021-03-06 23:47:40 +0000

    XXXXXXX

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,24 @@
 
 require "csv"
 
+User.destroy_all
+User.create(id: 0, name: 管理人, email: "q", password: "q", password_confirmation: "q")
+
+Game.destroy_all
+CSV.foreach('db/table_game.csv') do |row|
+  Game.create(id: row[0], date: row[1], start: row[2], home_id: row[3], visitor_id: row[4], ground_id: row[5])
+end
+
+Ground.destroy_all
+CSV.foreach('db/table_ground.csv') do |row|
+  Ground.create(id: row[0], name: row[1])
+end
+
+Seatgroup.destroy_all
+CSV.foreach('db/table_seatgroup.csv') do |row|
+  Seatgroup.create(id: row[0], name: row[1], ground_id: row[2])
+end
+
 Team.destroy_all
 CSV.foreach('db/table_team.csv') do |row|
   Team.create(id: row[0], name: row[1])

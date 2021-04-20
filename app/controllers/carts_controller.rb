ecfa33ed3283f7175db7247dfabba72e9d96b0e7
kohei <msk_mkt_tdf_ohr@yahoo.co.jp>
2021-04-20 17:24:43 +0900

    XXXXXX

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -4,8 +4,8 @@ class CartsController < ApplicationController
     @ticket_ids.each do |ticket_id|
       @purchase = Purchase.new(ticket_id: ticket_id, user_id: session[:user_id])
       unless @purchase.save
-        Purchase.where(ticket_id: @ticket_ids).delete_all
-        return redirect_to cart_path(session[:user_id]), success: "#{ticket_id}で失敗"
+        Purchase.where(ticket_id: @ticket_ids, user_id: session[:user_id]).delete_all
+        return redirect_to cart_path(session[:user_id]), danger: "#{ticket_id}は既に売り切れです"
       end
     end
     Cart.where(user_id: session[:user_id]).delete_all
